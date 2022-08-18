5a62b7b2daeda0f5c4340a513bd4113a940ff9f0
MahiAlJawad <br.mahialjawad@gmail.com>
2022-08-18 21:56:14 +0600

    Added new VC Category and Coresponding Database table in CoreData

---
+++ Todoey/Controllers/CategoryViewController.swift
@@ -0,0 +1,103 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by Mahi Al Jawad on 18/8/22.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import CoreData
+import Foundation
+import UIKit
+
+class CategoryViewController: UITableViewController {
+    // items are part of database object i.e. NSManagedObject
+    private var categories = [Category]()
+    
+    private let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadItems()
+    }
+    
+    
+    @IBAction func AddCategoryPressed(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "Add new Category", message: nil, preferredStyle: .alert)
+        
+        var addTextField = UITextField()
+        
+        alert.addTextField { textField in
+            textField.placeholder = "Add Category"
+            addTextField = textField
+        }
+        
+        // MARK: Create in CoreData Database
+        
+        let addAction = UIAlertAction(title: "Add", style: .default) { action in
+            guard let text = addTextField.text, !text.isEmpty else { return }
+            
+            let category = Category(context: self.context)
+            
+            category.name = text
+            
+            self.categories.append(category)
+            self.saveItem()
+            
+            // Note: Load only the new item, don't reload the whole table
+            
+            // Calculate the latest row to load
+            let index = self.categories.count - 1
+            
+            // Calculate the indexPath
+            let indexPath = IndexPath(item: index, section: 0)
+            
+            // Insert the latest row only
+            self.tableView.insertRows(at: [indexPath], with: .automatic)
+            
+            // scroll to the last item
+            self.tableView.scrollToRow(at: indexPath, at: .top, animated: true)
+        }
+        
+        alert.addAction(addAction)
+        
+        present(alert, animated: true)
+    }
+}
+
+// MARK: TableView Methods
+extension CategoryViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categories.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "categoryCell", for: indexPath)
+        cell.textLabel?.text = categories[indexPath.row].name
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
+// MARK: CoreData helper methods
+
+extension CategoryViewController {
+    func saveItem() {
+        do {
+            try self.context.save()
+        } catch {
+            print("Error in saving item: \(error)")
+        }
+    }
+    
+    func loadItems(with request: NSFetchRequest<Category> = Category.fetchRequest()) {
+        do {
+            categories = try context.fetch(request)
+        } catch {
+            print("Error in fetching data from database: \(error)")
+        }
+    }
+}
--- Todoey/Controllers/TodoViewController.swift
+++ Todoey/Controllers/TodoViewController.swift
@@ -4,6 +4,9 @@ import CoreData
 class TodoViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    // items are part of database object i.e. NSManagedObject
     private var items = [Item]()
     
     private let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
@@ -12,8 +15,12 @@ class TodoViewController: UIViewController {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
+        searchBar.delegate = self
+        // MARK: READ in CoreData Database
+        loadItems()
         
-        print("FileManger directory: \(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))")
+                // Do any additional setup after loading the view.
+        print("FileManager directory: \(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))")
     }
     
     @IBAction func addButtonPressed(_ sender: Any) {
@@ -26,8 +33,11 @@ class TodoViewController: UIViewController {
             addTextField = textField
         }
         
+        // MARK: Create in CoreData Database
+        
         let addAction = UIAlertAction(title: "Add", style: .default) { action in
             guard let text = addTextField.text, !text.isEmpty else { return }
+            
             let item = Item(context: self.context)
             
             item.title = text
@@ -76,11 +86,17 @@ extension TodoViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         
+        // MARK: Update in CoreData Database
         items[indexPath.row].isChecked = !items[indexPath.row].isChecked
-        
         saveItem()
         // Reload only one row
         tableView.reloadRows(at: [indexPath], with: .automatic)
+        
+        // MARK: Delete from CoreData Database
+//        context.delete(items[indexPath.row])
+//        items.remove(at: indexPath.row)
+//        saveItem()
+//        tableView.reloadData()
     }
 }
 
@@ -93,5 +109,39 @@ extension TodoViewController {
         } catch {
             print("Error in saving item: \(error)")
         }
+    }
+    
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest()) {
+        do {
+            items = try context.fetch(request)
+        } catch {
+            print("Error in fetching data from database: \(error)")
+        }
+    }
+}
+
+// MARK: UISearchBar Functions
+
+extension TodoViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print("\(searchText) bar: \(searchBar.text)")
+        guard let searchText = searchBar.text, !searchText.isEmpty else {
+            loadItems()
+            tableView.reloadData()
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+            
+            return
+        }
+        
+        let request = Item.fetchRequest()
+        
+        request.predicate = NSPredicate(format: "title contains[cd] %@", searchText)
+        
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        
+        loadItems(with: request)
+        tableView.reloadData()
     }
 }
